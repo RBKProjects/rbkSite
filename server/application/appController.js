fa466ed1a8a0b6646e9d3ecdffa6ffbf19f3e7c5
semo94 <saleem.bakri94@gmail.com>
2017-06-17 04:17:39 +0300

    add app Model + CRL

---
+++ server/application/appController.js
@@ -0,0 +1,5 @@
+const appModel = require('./appModel.js');
+
+module.exports = {
+
+}
---
+++ server/application/appModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const AppSchema = new mongoose.Schema({
+  userID {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'User'
+  },
+  testStatus {
+    type : String
+  },
+  appStatus {
+    type : String
+  },
+  progress {
+    type : Number
+  }
+});
+
+const App = mongoose.model('application', AppSchema);
+modual.exports = App;
